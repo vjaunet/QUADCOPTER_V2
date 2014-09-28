98e7d91b609a0b75357bb531673181d19c3152b4
Vincent JAUNET <vincent.jaunet@hotmail.fr>
2014-09-28 13:35:47 +0200

    New Rpi to Arduino send Servo values working

--- ARDUINO/RX_ESC_HANDLER/rpi_to_servos/rpi_to_servos.ino
+++ ARDUINO/RX_ESC_HANDLER/rpi_to_servos/rpi_to_servos.ino
@@ -10,6 +10,9 @@
 //LED pin for checking
 #define LED_PIN 13
 
+//Number of servos
+#define SERVO_NUM 4
+
 // Assign your channel out pins
 #define FL_MOTOR_OUT_PIN 4
 #define FR_MOTOR_OUT_PIN 5
@@ -17,10 +20,7 @@
 #define BR_MOTOR_OUT_PIN 7
 
 //define Servo variables
-Servo FL_MOTOR;
-Servo FR_MOTOR;
-Servo BL_MOTOR;
-Servo BR_MOTOR;
+Servo MOTOR[SERVO_NUM];
 
 void setup()
 {
@@ -31,10 +31,10 @@ void setup()
   // attach servo objects, these will generate the correct 
   // pulses for driving Electronic speed controllers, servos or other devices
   // designed to interface directly with RC Receivers  
-  FL_MOTOR.attach(FL_MOTOR_OUT_PIN);
-  FR_MOTOR.attach(FR_MOTOR_OUT_PIN);
-  BL_MOTOR.attach(BL_MOTOR_OUT_PIN);
-  BR_MOTOR.attach(BR_MOTOR_OUT_PIN);
+  MOTOR[0].attach(FL_MOTOR_OUT_PIN);
+  MOTOR[1].attach(FR_MOTOR_OUT_PIN);
+  MOTOR[2].attach(BL_MOTOR_OUT_PIN);
+  MOTOR[3].attach(BR_MOTOR_OUT_PIN);
 
   // initialize i2c as slave
   // define call backs ofr I2C
@@ -45,10 +45,10 @@ void setup()
 
 void loop()
 {
+
 delay(10);	
 
 }
-
 
 
 void SetServos(int byteCount)
@@ -57,22 +57,24 @@ void SetServos(int byteCount)
 // the RPI shoud send 4 x 1 uint16_t (2 bytes) values
 // one for each PID updated Motor speed
 
+  union int_byt{
+    uint8_t b[2]; 
+    uint16_t i;
+  };    
+  
+  uint8_t trash;
+
   //Expected bytes to be received
-  uint16_t b[4];
+  union int_byt *rcv_data = new int_byt[SERVO_NUM];
 
   if (byteCount == 8)
     {
       while(Wire.available()) {
 	
-	for (int i=0;i<4;i++)
+	for (int i=0;i<SERVO_NUM;i++)
 	  {
-	    b[i]  = Wire.read() << 8; //lower bits
-	    b[i] |= Wire.read();      //upper bits
-	    
-	    Serial.print("Received value ");
-	    Serial.print(i);
-	    Serial.print(" = ");
-	    Serial.println(b[i]);
+	    rcv_data[i].b[0] = Wire.read(); //upper bits?
+	    rcv_data[i].b[1] = Wire.read(); //upper bits?
 	  }
       }
 	
@@ -80,19 +82,27 @@ void SetServos(int byteCount)
   else {
     //short read from master
     //Do nothing
-    Serial.println("short read from master");
-    Serial.print("Expected 8 bytes");
-    Serial.print("Received");
-    Serial.print(byteCount);
-    Serial.print("bytes");
+    // Serial.println("Short read from master :");
+    // Serial.print("Expected 8 bytes,");
+    // Serial.print("Received ");
+    // Serial.print(byteCount);
+    // Serial.println(" bytes");
+
+    return;
   }
 
   //Update servo values
-  FL_MOTOR.writeMicroseconds(b[0]);
-  FR_MOTOR.writeMicroseconds(b[1]);
-  BL_MOTOR.writeMicroseconds(b[2]);
-  BR_MOTOR.writeMicroseconds(b[3]);
+  for (int i=0;i<SERVO_NUM;i++)
+    {
+      	    // Serial.print("Received value ");
+	    // Serial.print(i);
+	    // Serial.print(" = ");
+	    // Serial.println(rcv_data[i].i);
 
+      MOTOR[i].writeMicroseconds(rcv_data[i].i);
+    }
+
+  return;
 
 }
 
