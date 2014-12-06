07d321c0a3e0acaaa0c3332b6b09c889883f9e6f
Vincent JAUNET <vincent.jaunet@hotmail.fr>
2014-12-06 20:08:20 +0100

    SPI back and fourt 4 channels working

--- ARDUINO/spi_arduino_2_rpi/spi_arduino_2_rpi.ino
+++ ARDUINO/spi_arduino_2_rpi/spi_arduino_2_rpi.ino
@@ -17,9 +17,6 @@
 #include <SPI.h>
 #include <Servo.h>
 
-//Define I2C variables
-#define SLAVE_ADDRESS 0x11
-
 //LED pin for checking
 #define LED_PIN 13
 
@@ -34,7 +31,6 @@
 #define YAW_IN_PIN 11
 #define PITCH_IN_PIN 10
 #define ROLL_IN_PIN 9
-
 
 // shared variables are updated by the ISR and read by loop.
 // In loop we immediatley take local copies so that the ISR can keep ownership of the
@@ -60,22 +56,26 @@ uint32_t ulRollStart;
 //Number of servos
 #define SERVO_NUM 4
 #define RC_MIN 1000
-
 // Assign your channel out pins
-#define FL_MOTOR_OUT_PIN A0
-#define FR_MOTOR_OUT_PIN A1
-#define BL_MOTOR_OUT_PIN A2
-#define BR_MOTOR_OUT_PIN A3
+#define FL_MOTOR_OUT_PIN 4
+#define FR_MOTOR_OUT_PIN 5
+#define BL_MOTOR_OUT_PIN 6
+#define BR_MOTOR_OUT_PIN 7
 
 //define Servo variables
 Servo MOTOR[SERVO_NUM];
 
 //define SPI data
-byte cmd;
 volatile union int_byt{
   uint8_t u8[2];
   uint16_t u16;
 } rc_data[4], esc_data[4];
+
+volatile byte rx_buf[SERVO_NUM*2];
+uint8_t pos=0;
+
+bool start=false;
+volatile bool update_servo=false;
 
 //setup function
 void setup()
@@ -134,6 +134,38 @@ void loop()
   rc_data[PITCH].u16 = unPitchInShared;
   rc_data[ROLL].u16 = unRollInShared;
 
+  if (unThrottleInShared < 1000 &
+      unYawInShared < 1200 &
+      unPitchInShared < 1200 &
+      unRollInShared <1200 ) {
+    start = !start;
+
+  }
+
+  //Update servo (ESC) if necessary
+  if (update_servo & start){
+    uint8_t ipos=0;
+    for (int i=0;i<SERVO_NUM;i++)
+      {
+	//process buffer values
+	for (int ibyte = 0;ibyte<2;ibyte++){
+	  esc_data[i].u8[ibyte] = rx_buf[ipos];
+	  ipos++;
+	}
+
+	//write ESC output
+	MOTOR[i].writeMicroseconds(esc_data[i].u16);
+      }
+
+    update_servo = false;
+
+  } else if (!start) {
+    for (int i=0;i<SERVO_NUM;i++)
+      {
+	MOTOR[i].writeMicroseconds(RC_MIN);
+      }
+  }
+
 }
 
 /*-----------------------
@@ -142,49 +174,69 @@ void loop()
 ISR (SPI_STC_vect)
 {
 
-    //grab a new command
-    cmd = SPDR;
-    switch (cmd){
-    case 10:
-      SPDR = rc_data[THR].u8[0];
-      break;
+  //grab a new command and process it
+  byte cmd = SPDR;
 
-    case 11:
-      SPDR = rc_data[THR].u8[1];
-      break;
+  if (cmd == 'S') {
+    //STOP do nothing : end of sending RC data
+    pos=0;
+    return;
+  }
 
-    case 20:
-      SPDR = rc_data[YAW].u8[0];
-      break;
+  if (cmd == 'P') {
+    update_servo = true;
+    pos=0;
+    return;
+  }
 
-    case 21:
-      SPDR = rc_data[YAW].u8[1];
-      break;
+  //push data into a byte buffer
+  rx_buf[pos++] = cmd;
 
-    case 30:
-      SPDR = rc_data[PITCH].u8[0];
-      break;
+  // 10-11 -> send 2bytes channel 1 value
+  // ...
+  // 40-41 -> send 2bytes channel 4 value
+  // ...
+  // 60-61 -> send 2bytes channel 6 value
+  switch (cmd){
+  case 10:
+    SPDR = rc_data[THR].u8[0];
+    break;
 
-    case 31:
-      SPDR = rc_data[PITCH].u8[1];
-      break;
+  case 11:
+    SPDR = rc_data[THR].u8[1];
+    break;
 
-    case 40:
-      SPDR = rc_data[ROLL].u8[0];
-      break;
+  case 20:
+    SPDR = rc_data[YAW].u8[0];
+    break;
 
-    case 41:
-      SPDR = rc_data[ROLL].u8[1];
-      break;
-    }
+  case 21:
+    SPDR = rc_data[YAW].u8[1];
+    break;
 
+  case 30:
+    SPDR = rc_data[PITCH].u8[0];
+    break;
+
+  case 31:
+    SPDR = rc_data[PITCH].u8[1];
+    break;
+
+  case 40:
+    SPDR = rc_data[ROLL].u8[0];
+    break;
+
+  case 41:
+    SPDR = rc_data[ROLL].u8[1];
+    break;
+ }
 
 }
 
 /*----------------------------------
   simple interrupt service routine
   for PWM measurements
------------------------------------*/
+*/
 void calcThrottle()
 {
   // if the pin is high, its a rising edge of the signal pulse, so lets record its value
